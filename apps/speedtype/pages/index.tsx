25b6c8baa4511ef2223dc51fe65d6a8b378b7586
Fulvia Mourikis <zum281@gmail.com>
2022-05-06 18:39:39 +0200

    test

--- apps/speedtype/pages/index.tsx
+++ apps/speedtype/pages/index.tsx
@@ -3,7 +3,6 @@ import { Button } from '@chakra-ui/react';
 import { Game } from '../components/Game';
 import { Timer } from '../components/Timer';
 import { useGameContext } from '../context/GameContext';
-import { Results } from '../components/Results';
 
 export const Index: NextPage = () => {
   const { playing, startGame } = useGameContext();
